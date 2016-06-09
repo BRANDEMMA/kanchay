6e1ebcaddea2609f2e2fa5343176e83658fb7ec6
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-06-09 13:59:01 -0500

    añadi galeria

---
+++ galeria.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<html lang="es">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width" />
+	<title>KANCHAY PERU</title>
+	<meta name="author" content="Brandemma" />
+	<meta name="description" content="" />
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (INICIA)  =====================-->
+
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" href="css/lightbox.min.css">
+
+	<!--================== 	ENLACE HOJA DE ESTILOS (TERMINA)  =====================-->
+
+	<!--================== 	SCRIPT PARA SLIDER (INICIA)  =====================-->
+
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+	<!--================== 	SCRIPT PARA SLIDER (TERMINA)  =====================-->
+
+	<!--================== 	ENLACE ICONO (INICIA)  =====================-->
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+	<!--================== 	ENLACE ICONO (TERMINA)  =====================-->
+
+</head>
+
+<body>
+	<header>
+		<?php
+		include 'menu.php';
+		?>
+	</header>
+
+
+	<div class="fondo-azulclaro">
+		<div class="titulo-servicios2"><img src="img/galeria-kanchay-peru.png"><h2>GALERIA</h2></div>
+		
+		
+
+		<div class="contenedor-nosotros">
+			
+			<a href="img/imagen1-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen1-kanchay-peru.jpg"></a>
+			<a href="img/imagen2-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen2-kanchay-peru.jpg"></a>
+			<a href="img/imagen3-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen3-kanchay-peru.jpg"></a>
+			<a href="img/imagen4-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen4-kanchay-peru.jpg"></a>
+			<a href="img/imagen5-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen5-kanchay-peru.jpg"></a>
+			<a href="img/imagen1-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen1-kanchay-peru.jpg"></a>
+			<a href="img/imagen2-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen2-kanchay-peru.jpg"></a>
+			<a href="img/imagen3-kanchay-peru.jpg" data-lightbox="example-set"><img class="img-galeria" src="img/imagen3-kanchay-peru.jpg"></a>
+			<script src="js/lightbox-plus-jquery.min.js"></script>
+
+		</div>
+
+		<?php
+		include 'contacto.php';
+		?>
+
+	</div>
+
+	<?php
+	include 'pie-pagina.php';
+	?>		
+</body>
+
+</html>
