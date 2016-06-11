1cb19199c711923581f84aba9783ad2321f6931e
rockerny <eegoavil@brandemma.com>
2016-06-11 09:25:11 -0500

    Arreglé el nosotros
    Ya esta listo el nosotros.

--- nosotros.php
+++ nosotros.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1" />
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width" />
-	<title>KANCHAY PERU</title>
+	<title>KANCHAY PERU - NOSOTROS</title>
 	<meta name="author" content="Brandemma" />
 	<meta name="description" content="" />
 
@@ -53,16 +53,16 @@
 				<p>Porque somos una empresa especialmente creada para ti, para darte una satisfacción total en los servicios que requieras; con la satisfacción de la oferta recibida, nos comprometemos a ser los mejores del mercado local; ofreciéndote los mejores profesionales para tu atención personalizada siempre, las 24 horas del día.</p>
 				<h3>Nuestra gente</h3>
 				<p>
-				La unión de dos profesionales de ramas diferentes del servicio al cliente originó K´anchay Peru: José Fernando Benaducci Otayza, Guía Oficial de Turismo, Gerente General de la empresa, teniendo 27 años de experiencia, en el trato directo al público usuario y visitante, en Turismo receptivo y local. Además, tuvo una participación muy importante en el entrenamiento docente a otros guías de turismo del mercado local Lima, al igual que numerosos contactos del medio turístico para la atención y distribución de su carrera. Jerry Rolando Lolandes Arbayza, Consultor Financiero, con sede en la ciudad de Miami, quien con el interés de nuestro patrimonio es el principal promotor de nuestro país y de la empresa en el extranjero.</p>
+				La unión de dos profesionales de ramas diferentes del servicio al cliente originó K´anchay Peru: <strong>José Fernando Benaducci Otayza</strong>, Guía Oficial de Turismo, Gerente General de la empresa, teniendo 27 años de experiencia, en el trato directo al público usuario y visitante, en Turismo receptivo y local. Además, tuvo una participación muy importante en el entrenamiento docente a otros guías de turismo del mercado local Lima, al igual que numerosos contactos del medio turístico para la atención y distribución de su carrera.<br><br><strong> Jerry Rolando Lolandes Arbayza</strong>, Consultor Financiero, con sede en la ciudad de Miami, quien con el interés de nuestro patrimonio es el principal promotor de nuestro país y de la empresa en el extranjero.</p>
 			</div>
 			<div class="clear"></div>
 
 			<div class="testimonio-persona2 testimonio-personas-espacio2">
 				<div class="foto"><img src="img/foto2-testimonio.jpg"></div>
 				<div class="texto">
-					<h3>AMERICO BARRIAL</h3>
-					<p><span>GERENTE GENERAL</span></p>
-					<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, esencialmente igual al original.</p>
+					<h3>José Fernando Benaducci Otayza,</h3>
+					<p><span>Guía Oficial de Turismo, Gerente General</span></p>
+					
 				</div>
 				<div class="clear"></div>
 			</div>
@@ -70,9 +70,9 @@
 			<div class="testimonio-persona2">
 				<div class="foto"><img src="img/foto4-testimonio.jpg"></div>
 				<div class="texto">
-					<h3>JOSE MIRANDA</h3>
-					<p><span>DIRECTOR</span></p>
-					<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, esencialmente igual al original.</p>
+					<h3>Jerry Rolando Lolandes Arbayza</h3>
+					<p><span>Consultor Financiero</span></p>
+					
 				</div>
 				<div class="clear"></div>
 			</div>
