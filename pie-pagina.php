aa368cec4d89d98144f398214a91ef89a7d2dca3
JFRZ7 <jose.francisco.romero.7@gmail.com>
2016-06-14 13:22:13 -0500

    añadido RUC

--- pie-pagina.php
+++ pie-pagina.php
@@ -1,7 +1,8 @@
 <?php
 ?>
 	<footer>
-		<p>Diseñado por <a href="http://brandemma.com/" target="_blank"><img src="img/logo-brandemma.png"></a></p>
+		<p>K´ANCHAY PERU S.A.C.&nbsp;&nbsp; - &nbsp;&nbsp; RUC: 20601016045<br>
+		Diseñado por <a href="http://brandemma.com/" target="_blank"><img src="img/logo-brandemma.png"></a></p>
 	</footer>	
 <?php
 ?>
